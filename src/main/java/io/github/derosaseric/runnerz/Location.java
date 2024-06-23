9a1943a47db016310a587ed7dd44c1d9bf7891e8
derosaseric <84354209+derosaseric@users.noreply.github.com>
2024-06-23 10:12:34 -0700

    Add new Record file Run and Enum file Location

---
+++ src/main/java/io/github/derosaseric/runnerz/Location.java
@@ -0,0 +1,5 @@
+package io.github.derosaseric.runnerz;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
---
+++ src/main/java/io/github/derosaseric/runnerz/Run.java
@@ -0,0 +1,12 @@
+package io.github.derosaseric.runnerz;
+
+import java.time.LocalDateTime;
+
+public record Run(
+    Integer id,
+    String title,
+    LocalDateTime startedOn,
+    LocalDateTime completedOn,
+    Integer miles,
+    Location location
+) {}
